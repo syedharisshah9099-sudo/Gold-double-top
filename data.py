b03527756974b7d11becaf394153ca96be5aab12
syedharisshah9099-sudo <syedharisshah9099@gmail.com>
2026-07-19 16:58:28 +0500

    data.py

---
+++ data.py
@@ -0,0 +1,15 @@
+import yfinance as yf
+
+def get_market_data():
+    data = yf.download(
+        tickers="GC=F",
+        period="1d",
+        interval="1m",
+        progress=False,
+        auto_adjust=True
+    )
+
+    if data.empty:
+        return None
+
+    return data.tail(45)
