18a8d8461825275b0768941fa3c8675f5d3d7415
syedharisshah9099-sudo <syedharisshah9099@gmail.com>
2026-07-19 17:00:39 +0500

    telegram.py

---
+++ telegram.py
@@ -0,0 +1,20 @@
+import requests
+from config import TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID
+
+def send_alert(message):
+    if not TELEGRAM_BOT_TOKEN or not TELEGRAM_CHAT_ID:
+        print("Telegram credentials are missing.")
+        return
+
+    url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
+
+    payload = {
+        "chat_id": TELEGRAM_CHAT_ID,
+        "text": message
+    }
+
+    try:
+        requests.post(url, json=payload, timeout=10)
+        print("Telegram alert sent.")
+    except Exception as e:
+        print("Telegram Error:", e)
